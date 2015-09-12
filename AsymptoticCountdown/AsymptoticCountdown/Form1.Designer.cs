45a4696d13d2bd71540b16fe05e6916e147cc088
Chris Akridge <cakridge2@gmail.com>
2015-09-11 23:35:21 -0400

    Asymptotic countdown app based on an XKCD comic

---
+++ AsymptoticCountdown/AsymptoticCountdown/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace AsymptoticCountdown
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.DTPStart = new System.Windows.Forms.DateTimePicker();
+			this.DTPEnd = new System.Windows.Forms.DateTimePicker();
+			this.LabelProgress = new System.Windows.Forms.Label();
+			this.LabelAsymptoticProgress = new System.Windows.Forms.Label();
+			this.TimerMain = new System.Windows.Forms.Timer(this.components);
+			this.TimerRate = new System.Windows.Forms.Timer(this.components);
+			this.ProgressOverall = new System.Windows.Forms.ProgressBar();
+			this.ProgressHundredth = new System.Windows.Forms.ProgressBar();
+			this.ProgressTenThousandth = new System.Windows.Forms.ProgressBar();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(34, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Start:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 38);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(30, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "End:";
+			// 
+			// DTPStart
+			// 
+			this.DTPStart.CustomFormat = "yyyy-MM-dd hh:mm:ss tt";
+			this.DTPStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+			this.DTPStart.Location = new System.Drawing.Point(53, 6);
+			this.DTPStart.Name = "DTPStart";
+			this.DTPStart.Size = new System.Drawing.Size(200, 22);
+			this.DTPStart.TabIndex = 2;
+			this.DTPStart.ValueChanged += new System.EventHandler(this.DTPStart_ValueChanged);
+			// 
+			// DTPEnd
+			// 
+			this.DTPEnd.CustomFormat = "yyyy-MM-dd hh:mm:ss tt";
+			this.DTPEnd.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+			this.DTPEnd.Location = new System.Drawing.Point(53, 31);
+			this.DTPEnd.Name = "DTPEnd";
+			this.DTPEnd.Size = new System.Drawing.Size(200, 22);
+			this.DTPEnd.TabIndex = 3;
+			this.DTPEnd.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+			// 
+			// LabelProgress
+			// 
+			this.LabelProgress.AutoSize = true;
+			this.LabelProgress.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.LabelProgress.Location = new System.Drawing.Point(12, 56);
+			this.LabelProgress.Name = "LabelProgress";
+			this.LabelProgress.Size = new System.Drawing.Size(164, 21);
+			this.LabelProgress.TabIndex = 4;
+			this.LabelProgress.Text = "Overall Progress: {0}%";
+			// 
+			// LabelAsymptoticProgress
+			// 
+			this.LabelAsymptoticProgress.AutoSize = true;
+			this.LabelAsymptoticProgress.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.LabelAsymptoticProgress.Location = new System.Drawing.Point(12, 77);
+			this.LabelAsymptoticProgress.Name = "LabelAsymptoticProgress";
+			this.LabelAsymptoticProgress.Size = new System.Drawing.Size(232, 21);
+			this.LabelAsymptoticProgress.TabIndex = 5;
+			this.LabelAsymptoticProgress.Text = "Asymptotic Progress: {0} {1} ago";
+			// 
+			// TimerMain
+			// 
+			this.TimerMain.Enabled = true;
+			this.TimerMain.Interval = 16;
+			this.TimerMain.Tick += new System.EventHandler(this.TimerMain_Tick);
+			// 
+			// TimerRate
+			// 
+			this.TimerRate.Enabled = true;
+			this.TimerRate.Tick += new System.EventHandler(this.TimerRate_Tick);
+			// 
+			// ProgressOverall
+			// 
+			this.ProgressOverall.Location = new System.Drawing.Point(16, 102);
+			this.ProgressOverall.Maximum = 10000;
+			this.ProgressOverall.Name = "ProgressOverall";
+			this.ProgressOverall.Size = new System.Drawing.Size(403, 23);
+			this.ProgressOverall.TabIndex = 6;
+			// 
+			// ProgressHundredth
+			// 
+			this.ProgressHundredth.Location = new System.Drawing.Point(16, 131);
+			this.ProgressHundredth.Name = "ProgressHundredth";
+			this.ProgressHundredth.Size = new System.Drawing.Size(403, 23);
+			this.ProgressHundredth.TabIndex = 7;
+			// 
+			// ProgressTenThousandth
+			// 
+			this.ProgressTenThousandth.Location = new System.Drawing.Point(16, 160);
+			this.ProgressTenThousandth.Name = "ProgressTenThousandth";
+			this.ProgressTenThousandth.Size = new System.Drawing.Size(403, 23);
+			this.ProgressTenThousandth.TabIndex = 8;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(431, 196);
+			this.Controls.Add(this.ProgressTenThousandth);
+			this.Controls.Add(this.ProgressHundredth);
+			this.Controls.Add(this.ProgressOverall);
+			this.Controls.Add(this.LabelAsymptoticProgress);
+			this.Controls.Add(this.LabelProgress);
+			this.Controls.Add(this.DTPEnd);
+			this.Controls.Add(this.DTPStart);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Asymptotic Countdown";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.DateTimePicker DTPStart;
+		private System.Windows.Forms.DateTimePicker DTPEnd;
+		private System.Windows.Forms.Label LabelProgress;
+		private System.Windows.Forms.Label LabelAsymptoticProgress;
+		private System.Windows.Forms.Timer TimerMain;
+		private System.Windows.Forms.Timer TimerRate;
+		private System.Windows.Forms.ProgressBar ProgressOverall;
+		private System.Windows.Forms.ProgressBar ProgressHundredth;
+		private System.Windows.Forms.ProgressBar ProgressTenThousandth;
+	}
+}
+
---
+++ AsymptoticCountdown/AsymptoticCountdown/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsymptoticCountdown
+{
+	public partial class Form1 : Form
+	{
+		private DateTime start;
+		private DateTime end;
+
+		private double lastYears;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void DTPStart_ValueChanged(object sender, EventArgs e)
+		{
+			start = DTPStart.Value;
+		}
+
+		private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+		{
+			end = DTPEnd.Value;
+		}
+
+		private void TimerMain_Tick(object sender, EventArgs e)
+		{
+			double progress = GetProgress();
+			double asymptoticProgress = GetAsymptoticYears(progress);
+			double rate = (asymptoticProgress - lastYears) * 60d;
+			lastYears = asymptoticProgress;
+
+			LabelProgress.Text = string.Format("Progress: {0:F8}%", progress * 100d);
+			LabelAsymptoticProgress.Text = string.Format("{0} ago ({1:F2} per second)", FormatYear(asymptoticProgress), rate);
+
+			double adjustedProgress = progress * 10000d;
+
+			if (adjustedProgress > 10000d)
+			{
+				TimerMain.Stop();
+				return;
+			}
+
+			ProgressOverall.Value = (int)adjustedProgress;
+			ProgressHundredth.Value = (int)(AntiTruncate(progress * 100d) * 100d);
+			ProgressTenThousandth.Value = (int)(AntiTruncate(progress * 10000d) * 100d);
+		}
+
+		private double GetProgress()
+		{
+			DateTime now = DateTime.Now;
+			TimeSpan nowToEnd = end - now;
+			TimeSpan startToEnd = end - start;
+
+			double result = (startToEnd - nowToEnd).TotalSeconds / startToEnd.TotalSeconds;
+			if (double.IsInfinity(result) || result < 0d) return 0d;
+			return result;
+		}
+
+		private double GetAsymptoticYears(double percentage)
+		{
+			double percentageCubed = percentage * percentage * percentage;
+			double exponentialTerm = 20.3444d * percentageCubed + 3d;
+
+			double eRaised = Math.Exp(exponentialTerm);
+			double eCubed = Math.Exp(3d);
+
+			return eRaised - eCubed;
+		}
+
+		private string FormatYear(double years)
+		{
+			if (years * 365.25d * 24d * 60d < 1d)
+			{
+				return string.Format("{0:F2} seconds", years * 365.25d * 24d * 60d * 60d);
+			}
+			else if (years * 365.25d * 24d < 1d)
+			{
+				return string.Format("{0:F2} minutes", years * 365.25d * 24d * 60d);
+			}
+			else if (years * 365.25d < 1d)
+			{
+				return string.Format("{0:F2} hours", years * 365.25d * 24d);
+			}
+			else if (years < 1d)
+			{
+				return string.Format("{0:F2} days", years * 365.25d);
+			}
+			else
+			{
+				return string.Format("{0:F2} years", years);
+			}
+		}
+
+		private void TimerRate_Tick(object sender, EventArgs e)
+		{
+			
+		}
+
+		private static double AntiTruncate(double value)
+		{
+			return (value - Math.Truncate(value));
+		}
+	}
+}
