62cf82313bf27bea4ab17ac23b5f817e5b2c5699
Chris Akridge <cakridge2@gmail.com>
2017-05-22 11:26:17 -0400

    Added notes for integrating NEAT-style evolution

--- EvoANTCore/WorldSettings.cs
+++ EvoANTCore/WorldSettings.cs
@@ -43,18 +43,9 @@ namespace EvoANTCore
 			MaxRandomConnectionsBetweenNeurons = 5;
 		}
 
-		// WYLO: next up: breeding
-		// 1. select the top n ants by lifespan from a generation (where N is even)
-		// 2. pair each ant in the top group randomly
-		// 3. given two ants,
-		//	*. create a new ant with no hidden neurons
-		//	a. select half the hidden neurons at random
-		//	b. extract them into a separate collection
-		//	c. sever outbound connections to neurons not in the new collection
-		//	d. keep connections to neurons that are in the new collection or to output neurons
-		//	e. combine the neurons from the two collections (one collection) from each ant
-		//	f. spawn new connections between the two groups
-		//	g. rewire connections to new output neurons and from the new input neurons
-		// 4. any ants not made from breeding will be randomly generated
+		// WYLO: next up: better breeding
+		// idunnolo? maybe do some research
+		// http://stackoverflow.com/questions/31708478/how-to-evolve-weights-of-a-neural-network-in-neuroevolution
+		// https://medium.com/@harvitronix/lets-evolve-a-neural-network-with-a-genetic-algorithm-code-included-8809bece164
 	}
 }
