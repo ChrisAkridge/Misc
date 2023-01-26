6c470ea8d39bc9687284c579916a372ec49541d6
Chris Akridge <cakridge2@gmail.com>
2023-01-25 22:20:02 -0500

    Start the season generator

---
+++ Celarix.JustForFun.FootballSimulator/Celarix.JustForFun.FootballSimulator/ScheduleGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Celarix.JustForFun.FootballSimulator.Data.Models;
+
+namespace Celarix.JustForFun.FootballSimulator
+{
+    internal static class ScheduleGenerator
+    {
+        public sealed class DivisionMatchup
+        {
+            public Conference ConferenceA { get; set; }
+            public Division DivisionA { get; set; }
+            public Conference ConferenceB { get; set; }
+            public Division DivisionB { get; set; }
+        }
+        
+        public static List<GameRecord> GeneratePreseasonAndRegularSeason(List<Team> teams,
+            int calendarYear)
+        {
+            
+        }
+
+        private static List<DateTimeOffset> GetRegularSeasonWeekStartDatesForYear(int calendarYear)
+        {
+            // Week 1 starts on the second Thursday of September
+            
+        }
+    }
+}
