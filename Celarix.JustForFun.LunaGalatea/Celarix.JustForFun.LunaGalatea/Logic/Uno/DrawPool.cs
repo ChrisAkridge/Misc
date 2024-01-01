bc9c17cd3194bd3c1c9a997a2647304e49314f9a
Chris Akridge <chrisa@carsquickbuy.com>
2024-01-01 17:04:43 -0500

    Actually start Uno player

---
+++ Celarix.JustForFun.LunaGalatea/Celarix.JustForFun.LunaGalatea/Logic/Uno/DrawPool.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celarix.JustForFun.LunaGalatea.Logic.Uno
+{
+    internal static class DrawPool
+    {
+        private static Card[] cards = Array.Empty<Card>();
+        public static IReadOnlyList<Card> Cards => cards;
+
+        public static void Fill()
+        {
+            var numberRange = Enumerable.Range(0, 10);
+            var symbolRange = new[]
+            {
+                CardSymbol.DrawTwo,
+                CardSymbol.Reverse,
+                CardSymbol.Skip
+            };
+            var colorRange = new[]
+            {
+                CardColor.Red,
+                CardColor.Green,
+                CardColor.Blue,
+                CardColor.Yellow
+            };
+
+            // https://stackoverflow.com/a/20870526
+            // firstList.Join(secondList, x => true, y => true, (m, n) => new { m, n });
+            var numberCards = RepeatEachItem(numberRange, 2)
+                .Join(colorRange, _ => true, _ => true, (n, c) => new { n, c })
+                .Select(nc => new Card
+                {
+                    Number = nc.n,
+                    Color = nc.c
+                });
+            var symbolCards = RepeatEachItem(symbolRange, 2)
+                .Join(colorRange, _ => true, _ => true, (s, c) => new { s, c })
+                .Select(sc => new Card
+                {
+                    Symbol = sc.s,
+                    Color = sc.c
+                });
+
+            cards = numberCards
+                .Concat(symbolCards)
+                .Concat(Enumerable.Repeat(new Card
+                {
+                    Color = CardColor.Wild
+                }, 4))
+                .Concat(Enumerable.Repeat(new Card
+                {
+                    Color = CardColor.Wild,
+                    Symbol = CardSymbol.WildDrawFour
+                }, 4))
+                .ToArray();
+        }
+
+        private static IEnumerable<T> RepeatEachItem<T>(IEnumerable<T> sequence, int times)
+        {
+            foreach (T item in sequence)
+            {
+                for (int i = 0; i < times; i++)
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ Celarix.JustForFun.LunaGalatea/Celarix.JustForFun.LunaGalatea/Logic/Uno/Enums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celarix.JustForFun.LunaGalatea.Logic.Uno
+{
+    public enum CardColor
+    {
+        Red,
+        Green,
+        Yellow,
+        Blue,
+        Wild
+    }
+
+    public enum CardSymbol
+    {
+        Reverse,
+        Skip,
+        DrawTwo,
+        WildDrawFour
+    }
+
+    public enum PlayDirection
+    {
+        Clockwise,
+        Counterclockwise
+    }
+}
