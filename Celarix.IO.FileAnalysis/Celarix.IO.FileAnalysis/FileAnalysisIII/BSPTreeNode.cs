a45f04b18e43c6dbcb08d5a1a2a5ddf2f00e7628
Chris Akridge <cakridge2@gmail.com>
2024-11-17 02:46:56 -0500

    Still too slow

--- Celarix.IO.FileAnalysis/Celarix.IO.FileAnalysis/FileAnalysisIII/BSPTreeNode.cs
+++ Celarix.IO.FileAnalysis/Celarix.IO.FileAnalysis/FileAnalysisIII/BSPTreeNode.cs
@@ -15,35 +15,21 @@ namespace Celarix.IO.FileAnalysis.FileAnalysisIII
 		IMinMaxValue<T>
 	{
 #nullable enable
-		private static readonly KeyValuePair<T, int>[] treeSplitBuffer = new KeyValuePair<T, int>[5];
-		private static readonly T?[] valueSearchBuffer = new T?[4];
-
-		private BSPTreeNode<T>? left;
-		private BSPTreeNode<T>? right;
-
-		private KeyValuePair<T, int>? _0;
-		private KeyValuePair<T, int>? _1;
-		private KeyValuePair<T, int>? _2;
-		private KeyValuePair<T, int>? _3;
+		private const int MaxLeafValues = 256;
+		
 		private readonly T rangeMinimum = rangeMinimum;
 		private readonly T rangeMaximum = rangeMaximum;
 
-		public int Count
-		{
-			get
-			{
-				if (left != null)
-				{
-					return left.Count + right!.Count;
-				}
-				
-				if (_3 != null) { return 4; }
-				if (_2 != null) { return 3; }
-				if (_1 != null) { return 2; }
-				if (_0 != null) { return 1; }
-				return 0;
-			}
-		}
+		private KeyValuePair<T, int>?[]? leafValues = new KeyValuePair<T, int>?[MaxLeafValues];
+		private static readonly KeyValuePair<T, int>[] treeSplitBuffer = new KeyValuePair<T, int>[MaxLeafValues + 1];
+		private int leafValueCount;
+		private BSPTreeNode<T>? left;
+		private BSPTreeNode<T>? right;
+		
+		public int Count =>
+			left != null
+				? left.Count + right!.Count
+				: leafValueCount;
 
 		public void Add(T item, int count = 1)
 		{
@@ -59,54 +45,39 @@ namespace Celarix.IO.FileAnalysis.FileAnalysisIII
 					right.Add(item, count);
 				}
 			}
-			else if (Count <= 4)
+			else if (Count <= MaxLeafValues)
 			{
 				// This is a leaf node and contains the actual values. Either find it or add it.
-				// First, do a null check.
-				Array.Clear(valueSearchBuffer);
-				if (_0 != null) { valueSearchBuffer[0] = _0!.Value.Key; }
-				if (_1 != null) { valueSearchBuffer[1] = _1!.Value.Key; }
-				if (_2 != null) { valueSearchBuffer[2] = _2!.Value.Key; }
-				if (_3 != null) { valueSearchBuffer[3] = _3!.Value.Key; }
-				var foundIndex = Array.IndexOf(valueSearchBuffer, item);
+				int foundIndex = -1;
+				for (var i = 0; i < leafValueCount; i++)
+				{
+					if (leafValues![i]!.Value.Key.CompareTo(item) != 0) { continue; }
+
+					foundIndex = i;
+					break;
+				}
+				
 				if (foundIndex != -1)
 				{
 					// We found the item. Increment the count.
-					switch (foundIndex)
-					{
-						case 0:
-							_0 = new KeyValuePair<T, int>(_0!.Value.Key, _0.Value.Value + count);
-							break;
-						case 1:
-							_1 = new KeyValuePair<T, int>(_1!.Value.Key, _1.Value.Value + count);
-							break;
-						case 2:
-							_2 = new KeyValuePair<T, int>(_2!.Value.Key, _2.Value.Value + count);
-							break;
-						case 3:
-							_3 = new KeyValuePair<T, int>(_3!.Value.Key, _3.Value.Value + count);
-							break;
-					}
+					leafValues![foundIndex] = new KeyValuePair<T, int>(leafValues[foundIndex]!.Value.Key, leafValues[foundIndex]!.Value.Value + count);
 				}
 				else
 				{
 					// We didn't find the item. Try to add it.
-					if (Count < 4)
+					if (Count < MaxLeafValues)
 					{
-						if (_0 == null) { _0 = new KeyValuePair<T, int>(item, count); }
-						else if (_1 == null) { _1 = new KeyValuePair<T, int>(item, count); }
-						else if (_2 == null) { _2 = new KeyValuePair<T, int>(item, count); }
-						else if (_3 == null) { _3 = new KeyValuePair<T, int>(item, count); }
+						leafValues![Count] = new KeyValuePair<T, int>(item, count);
+						leafValueCount++;
 					}
 					else
 					{
 						// We have reached the limit of this leaf node. We need to split it.
-						treeSplitBuffer[0] = _0!.Value;
-						treeSplitBuffer[1] = _1!.Value;
-						treeSplitBuffer[2] = _2!.Value;
-						treeSplitBuffer[3] = _3!.Value;
-						treeSplitBuffer[4] = new KeyValuePair<T, int>(item, count);
-						
+						for (var i = 0; i < MaxLeafValues; i++)
+						{
+							treeSplitBuffer[i] = leafValues![i]!.Value;
+						}
+						treeSplitBuffer[MaxLeafValues] = new KeyValuePair<T, int>(item, count);
 						Array.Sort(treeSplitBuffer, (a, b) => a.Key.CompareTo(b.Key));
 						var leftRangeMaximum = ((rangeMaximum - rangeMinimum) / 2) + rangeMinimum;
 						var rightRangeMinimum = leftRangeMaximum + 1;
@@ -114,7 +85,7 @@ namespace Celarix.IO.FileAnalysis.FileAnalysisIII
 						left = new BSPTreeNode<T>(rangeMinimum, leftRangeMaximum);
 						right = new BSPTreeNode<T>(rightRangeMinimum, rangeMaximum);
 						
-						for (var i = 0; i < 5; i++)
+						for (var i = 0; i < MaxLeafValues + 1; i++)
 						{
 							var itemToAdd = treeSplitBuffer[i];
 							if (itemToAdd.Key.CompareTo(leftRangeMaximum) <= 0)
@@ -126,12 +97,8 @@ namespace Celarix.IO.FileAnalysis.FileAnalysisIII
 								right.Add(itemToAdd.Key, itemToAdd.Value);
 							}
 						}
-						
-						// Clear out the leaf node.
-						_0 = null;
-						_1 = null;
-						_2 = null;
-						_3 = null;
+
+						leafValues = null;
 					}
 				}
 			}
