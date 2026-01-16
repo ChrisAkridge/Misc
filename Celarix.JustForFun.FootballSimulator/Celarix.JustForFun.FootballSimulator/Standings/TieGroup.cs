368b711b9faf7a3054dd04d7684e60d93c7a9f12
ChrisAkridge <cakridge2@gmail.com>
2026-01-15 22:48:16 -0500

    Start a better tiebreaker system

---
+++ Celarix.JustForFun.FootballSimulator/Celarix.JustForFun.FootballSimulator/Standings/TieGroup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Celarix.JustForFun.FootballSimulator.Standings
+{
+    public sealed class TieGroup : IComparable<TieGroup>
+    {
+        private readonly List<TeamStats> tiedTeams;
+
+        public int Position { get; }
+        public IReadOnlyList<TeamStats> TiedTeams => tiedTeams.AsReadOnly();
+
+        public TieGroup(int position)
+        {
+            Position = position;
+            tiedTeams = [];
+        }
+
+        public int CompareTo(TieGroup? other)
+        {
+            if (other is null) return 1;
+            return Position.CompareTo(other.Position);
+        }
+    }
+}
