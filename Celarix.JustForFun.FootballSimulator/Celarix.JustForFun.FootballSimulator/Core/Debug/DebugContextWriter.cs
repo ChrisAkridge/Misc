015596e0bbc9e9b0b0a910b80b42d54bfacbeb12
ChrisAkridge <cakridge2@gmail.com>
2026-02-03 18:10:51 -0500

    Add rest of system state machine tests

---
+++ Celarix.JustForFun.FootballSimulator/Celarix.JustForFun.FootballSimulator/Core/Debug/DebugContextWriter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Celarix.JustForFun.FootballSimulator.Core.Debug
+{
+    internal sealed class DebugContextWriter
+    {
+        private bool enabled;
+        private string folderPath;
+        private StreamWriter? streamWriter;
+
+        public DebugContextWriter(bool enabled, string folderPath)
+        {
+            this.enabled = enabled;
+            this.folderPath = folderPath;
+            if (enabled)
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+        }
+
+        public void StartNewGame(int gameID)
+        {
+            if (streamWriter != null)
+            {
+                streamWriter.Flush();
+                streamWriter.Close();
+                streamWriter.Dispose();
+                streamWriter = null;
+            }
+
+            var now = DateTimeOffset.Now.ToString("yyyyMMdd_hhmmss");
+            var filePath = Path.Combine(folderPath, $"Game_{gameID}_{now}.log");
+            streamWriter = new StreamWriter(filePath, false, Encoding.UTF8);
+        }
+    }
+}
